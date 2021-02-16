bff569fb5596484ff1b40acf967d9b0105b0289b
PalomaLP <palomaledesmap@gmail.com>
2021-02-16 19:29:33 +0100

    firstversion

---
+++ curriculum_vitae/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="personalInformation">
+        <h1 className="myName">Paloma Ledesma Peña</h1>
+        <h3 className= "profession">Trabajadora Social</h3>
+        <h4 className= "introduction">Soy una trabajadora social con aspiraciones de FrontEnd</h4>
+      </div>
+    <div className= "buttonInformation">
+        <button className="formacion">Formación</button>
+        <button className="experienciaProfesional">Experiencia profesional</button>
+        <button className= "objetivosProfesionales">Objetivos profesionales</button>
+        <button className= "otros">Otros</button>
+        <button className= "contacto">Contacto</button>
+    </div>
+    </div>
+  );
+}
+
+export default App;
